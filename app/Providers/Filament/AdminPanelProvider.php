cff0701faec5652136200d1fe673b980dcfbcc4f
Carlos Bailey <cbailey@Carloss-MacBook-Pro.local>
2026-01-03 01:10:08 -0500

    Update brand name to Data Analysis

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->brandName('DASHDINA Analytics')
+            ->brandName('Data Analysis')
             ->font('Outfit')
             ->sidebarCollapsibleOnDesktop()
             ->colors([
